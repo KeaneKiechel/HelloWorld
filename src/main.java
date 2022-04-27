f5fb31fff206665d17aca19cf114df8f13939fed
Keane Kiechel B Tumanda <keanekiechel.tumanda@g.msuiit.edu.ph>
2022-04-28 00:15:40 +0800

    Added attributes

--- src/main.java
+++ src/main.java
@@ -5,6 +5,12 @@ public class main {
 		student st1 = new student();
 		teacher t1 = new teacher();
 		
+		st1.name = "Al-Raffy Sarip";
+		st1.idNumber = "2020-0509";
+		
+		t1.name = "Yassin Mohammad";
+		t1.idNumber = "2020-0069";
+		
 	}
 }
 
